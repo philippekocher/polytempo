261d0965c6413962838dc78f2694ca604a6da154
Philippe Kocher <philippe.kocher@zhdk.ch>
2017-12-20 17:57:37 +0100

    layout issues

--- Source/Views/PolytempoComposer/Polytempo_ComposerMainView.cpp
+++ Source/Views/PolytempoComposer/Polytempo_ComposerMainView.cpp
@@ -120,7 +120,7 @@ void Polytempo_ComposerMainView::setLeftComponent(componentType type)
     
     float position = float(stretchableManager.getItemCurrentPosition(1)) / getWidth();
     if(position > 0 && position < 1)
-        resizerBarPosition = float(stretchableManager.getItemCurrentPosition(1)) / getWidth(); // to restore it later
+        resizerBarPosition = float(stretchableManager.getItemCurrentPosition(1)) / getWidth() - stretchableManager.getItemCurrentRelativeSize(1); // to restore it later
     
     if(leftComponentType == componentType_None)
     {
@@ -135,7 +135,7 @@ void Polytempo_ComposerMainView::setLeftComponent(componentType type)
     else
     {
         stretchableManager.setItemLayout(0,-0.3,-0.7,-resizerBarPosition);
-        stretchableManager.setItemLayout(2,-0.3,-0.7,resizerBarPosition-1);
+        stretchableManager.setItemLayout(2,-0.3,-0.7,resizerBarPosition-1.0);
     }
     
     if(leftComponentType == componentType_TimeMap)       leftComponent.addAndMakeVisible(timeMapComponent);
@@ -160,8 +160,8 @@ void Polytempo_ComposerMainView::setRightComponent(componentType type)
 
     float position = float(stretchableManager.getItemCurrentPosition(1)) / getWidth();
     if(position > 0 && position < 1)
-        resizerBarPosition = float(stretchableManager.getItemCurrentPosition(1)) / getWidth(); // to restore it later
-    
+        resizerBarPosition = float(stretchableManager.getItemCurrentPosition(1)) / getWidth() - stretchableManager.getItemCurrentRelativeSize(1); // to restore it later
+
     if(rightComponentType == componentType_None)
     {
         stretchableManager.setItemLayout(0,-1.0,-1.0,-1.0);
