fe4997c717d79d194f81a40f19c6eb6424b1f745
Philippe Kocher <philippe.kocher@zhdk.ch>
2021-05-11 15:15:18 +0200

    tidying up

--- Source/Application/Polytempo_LatestVersionChecker.h
+++ Source/Application/Polytempo_LatestVersionChecker.h
@@ -17,26 +17,14 @@ private:
     void run() override;
     bool fetchFromUpdateServer();
     bool newerVersionAvailable();
-//    void askUserAboutNewVersion (const String&, const String&, const VersionInfo::Asset&);
-//    void askUserForLocationToDownload (const VersionInfo::Asset&);
-//    void downloadAndInstall (const VersionInfo::Asset&, const File&);
-//
-//    void showDialogWindow (const String&, const String&, const VersionInfo::Asset&);
-//    void addNotificationToOpenProjects (const VersionInfo::Asset&);
 
     bool showAlert = false;
     String versionString;
     String releaseNotes;
-    
     struct Asset
     {
         const String name;
         const String url;
     };
-    
     std::vector<Asset> parsedAssets;
-
-//
-//    std::unique_ptr<DownloadAndInstallThread> installer;
-//    std::unique_ptr<Component> dialogWindow;
 };
