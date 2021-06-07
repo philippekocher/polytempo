b82478defd9837c4402b99f1d0a65955e91d6440
Philippe Kocher <philippe.kocher@zhdk.ch>
2021-06-07 12:40:29 +0200

    default preferences: user-friendlier zoom settings for time map and tempo map

--- Source/Preferences/Polytempo_DefaultPreferences.cpp
+++ Source/Preferences/Polytempo_DefaultPreferences.cpp
@@ -4,8 +4,8 @@ Polytempo_DefaultPreferences::Polytempo_DefaultPreferences()
 {
     // composer
     setValue("zoomX", 50);
-    setValue("timeMapZoomY", 20);
-    setValue("tempoMapZoomY", 20);
+    setValue("timeMapZoomY", 40);
+    setValue("tempoMapZoomY", 700);
     setValue("mainWindowContent", "0.5 1 3");
     setValue("tempoMeasurement", "1/4 60");
     setValue("defaultBeatPattern", "4/4 1");
