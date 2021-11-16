083ff48ae6421a9de40642c5d948a50152e9516a
Christian Schweizer <christian@schweizerweb.ch>
2021-11-16 14:42:13 +0100

    a step forward with mxe

--- MaxMSP/mxe/source/projects/polytemponetwork/polytemponetwork.cpp
+++ MaxMSP/mxe/source/projects/polytemponetwork/polytemponetwork.cpp
@@ -4,28 +4,19 @@
 
 using namespace c74::min;
 
+class polytemponetwork;
+
 class MyHandler : public EventCallbackHandler, public TickCallbackHandler, public StateCallbackHandler
 {
 public:
+    MyHandler(polytemponetwork* pExternal);
     void processEvent(std::string const& message) override;
     void processTick(double tick) override;
     void processState(int state, std::string message) override;
+    
+private:
+    polytemponetwork* m_pExternal;
 };
-
-void MyHandler::processEvent(std::string const& message)
-{
-    std::cout << "Event: " << message << std::endl;
-}
-
-void MyHandler::processTick(double tick)
-{
-    std::cout << "Tick: " << tick << std::endl;
-}
-
-void MyHandler::processState(int state, std::string message)
-{
-    std::cout << "State: " << state << ": " << message << std::endl;
-}
 
 class polytemponetwork : public object<polytemponetwork> {
 public:
@@ -36,8 +27,57 @@ public:
 
     inlet<>  input	{ this, "(bang) post greeting to the max console" };
     outlet<> output	{ this, "(anything) output the message which is posted to the max console" };
-    MyHandler handler;
-
+    MyHandler* pHandler;
+    bool m_initialized { false };
+    
+    polytemponetwork(const atoms& args = {}) {
+        /*
+        if (args.size() > 0)
+            frequency = args[0];
+        if (args.size() > 1)
+            resonance = args[1];
+        */
+        
+        if(!dummy())
+        {
+            cout << "init" << endl;
+            if(m_initialized)
+            {
+                cout << "deinit" << endl;
+                polytempo_release();
+            }
+            
+            int ret = polytempo_initialize(47522, false);
+            if(ret == 0)
+            {
+                pHandler = new MyHandler(this);
+                cout << "register event callback: " << pHandler << "." << endl;
+                polytempo_registerEventCallback(pHandler);
+                string name = args.size() > 0 ? args[0] : "MaxExternal";
+                polytempo_sendEvent("settings name " + name);
+                m_initialized = true;
+            }
+            else
+            {
+                cout << "error initializing polytempo lib" << endl;
+            }
+        }
+    }
+    
+    void logMessage(string msg)
+    {
+        cout << msg << endl;
+    }
+    
+    ~polytemponetwork() {
+        if(m_initialized && !dummy())
+        {
+            cout << "deinit" << endl;
+            m_initialized = false;
+            polytempo_release();
+        }
+    }
+    
     // define an optional argument for setting the message
     argument<symbol> greeting_arg { this, "greeting", "Initial value for the greeting attribute.",
         MIN_ARGUMENT_FUNCTION {
@@ -71,19 +111,37 @@ public:
     // post to max window == but only when the class is loaded the first time
     message<> maxclass_setup { this, "maxclass_setup",
         MIN_FUNCTION {
-            cout << "init" << endl;
-            polytempo_initialize(47522, false);
-            //cout << "register event callback" << endl;
-            //polytempo_registerEventCallback(&handler);
             //cout << "register state callback" << endl;
             //polytempo_registerStateCallback(this);
             //cout << "register tick callback" << endl;
             //polytempo_registerTickCallback(&handler);
-            polytempo_sendEvent("settings name MaxExternal");
+            
             return {};
         }
     };
+            
 };
+
+MyHandler::MyHandler(polytemponetwork* pExternal)
+{
+    m_pExternal = pExternal;
+}
+
+void MyHandler::processEvent(std::string const& message)
+{
+    std::cout << "Event: " << message << std::endl;
+    m_pExternal->logMessage(message);
+}
+
+            void MyHandler::processTick(double tick)
+            {
+                std::cout << "Tick: " << tick << std::endl;
+            }
+
+            void MyHandler::processState(int state, std::string message)
+            {
+                std::cout << "State: " << state << ": " << message << std::endl;
+            }
 
 
 MIN_EXTERNAL(polytemponetwork);
